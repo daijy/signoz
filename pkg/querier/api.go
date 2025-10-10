8d4121e93aa5bed38d9b697baadb1dfae498b116
Daniel Dai <jidai@roku.com>
2025-10-09 21:43:56 -0700

    Better logging

--- pkg/querier/api.go
+++ pkg/querier/api.go
@@ -29,7 +29,6 @@ func NewAPI(set factory.ProviderSettings, querier Querier, analytics analytics.A
 
 func (a *API) QueryRange(rw http.ResponseWriter, req *http.Request) {
 
-	panic("jidai catch you")
 	ctx := req.Context()
 
 	claims, err := authtypes.ClaimsFromContext(ctx)
@@ -76,6 +75,7 @@ func (a *API) QueryRange(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	a.set.Logger.Info("jidai catch you")
 	queryRangeResponse, err := a.querier.QueryRange(ctx, orgID, &queryRangeRequest)
 	if err != nil {
 		render.Error(rw, err)
