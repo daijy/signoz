5d2f1644d0c35436033e3fdf7c5b2e3f31369942
Daniel Dai <jidai@roku.com>
2025-10-13 14:24:48 -0700

    Better logging

--- pkg/telemetrymetadata/metadata.go
+++ pkg/telemetrymetadata/metadata.go
@@ -653,6 +653,9 @@ func (t *telemetryMetaStore) getMetricsKeys(ctx context.Context, fieldKeySelecto
 	query, args := mainSb.BuildWithFlavor(sqlbuilder.ClickHouse)
 
 	log.Printf("jidai getMetricsKeys %s", query)
+	for _, arg := range args {
+		log.Printf("jidai arg %v", arg)
+	}
 	rows, err := t.telemetrystore.ClickhouseDB().Query(ctx, query, args...)
 	if err != nil {
 		return nil, false, errors.Wrapf(err, errors.TypeInternal, errors.CodeInternal, ErrFailedToGetMetricsKeys.Error())
